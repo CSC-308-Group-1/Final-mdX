b3d72966ced54b39fe9771937c969a36a6d79955
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 11:52:50 -0700

    Update defines.sv

--- defines.sv
+++ defines.sv
@@ -1,8 +1,7 @@
 `timescale 1ns/1ps
 
-
 `define SMEM_MAX 1024
 `define RMEM_MAX 256
-`define TRANSACTION_COUNT 1
-`define DRIV_IF mem_intf.ME_DRIVER.ME_driver_cb
+`define TRANSACTION_COUNT 5
+`define DRIV_IF mem_intf.DriverInterface.Driver_cb
 `define MON_IF mem_intf.ME_MONITOR.ME_monitor_cb
