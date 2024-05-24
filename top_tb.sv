64a847626c0a5aa547899927de81a5b3816de8fd
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 12:06:58 -0700

    Update top_tb.sv

--- top_tb.sv
+++ top_tb.sv
@@ -10,7 +10,7 @@ module top_tb();
   always #10 clk = ~clk;  // Clock Generation
   
   initial begin 
-    $display(" ================================================= TB Start = 0 =================================================\n");
+    $display("%%%%% [ENV_INFO] Testbench Start %%%%%");
     memoryInterface.start = 1'b0;
     repeat(2) @(posedge clk);
     memoryInterface.start = 1'b1;
