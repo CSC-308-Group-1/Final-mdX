af3e100b6bde569b3a3ae24b69603441d0e7c337
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 12:00:14 -0700

    Update test.sv

--- test.sv
+++ test.sv
@@ -2,14 +2,14 @@
 
 `include "environment.sv"
 
-program test(MotionEstimationInterface mem_intf); 
+program test(MotionEstimationInterface memoryInterface); 
 
   // Instance of the environment class
   environment env;
   
   // Initial block to set up and run the environment
   initial begin
-    env = new(mem_intf);  // Create a new environment instance with the given interface
+    env = new(memoryInterface);  // Create a new environment instance with the given interface
     env.gen.trans_count = `TRANSACTION_COUNT;  // Set the total number of transactions to be generated
     env.run();  // Start the run task of the environment
   end
