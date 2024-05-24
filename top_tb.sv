a584b0a1b762e88fe1692a75ff6d7b526fee7e03
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 10:55:46 -0700

    Update top_tb.sv

--- top_tb.sv
+++ top_tb.sv
@@ -6,32 +6,32 @@
 `include "assertion.sv"
 
 module top_tb();
-  bit               clk;  
-  always #10 clk = ~clk;                 // Clock Generation
+  bit clk;
+  always #10 clk = ~clk;  // Clock Generation
   
   initial begin 
     $display(" ================================================= TB Start = 0 =================================================\n");
     mem_intf.start = 1'b0;
-    repeat(2)
-    @(posedge clk);   
+    repeat(2) @(posedge clk);
     mem_intf.start = 1'b1;
   end
   
-  ME_interface mem_intf(clk);           // Interface Instantiation
+  ME_interface mem_intf(clk);  // Interface Instantiation
   ROM_R memR_u(.clock(clk), .AddressR(mem_intf.AddressR), .R(mem_intf.R));
   ROM_S memS_u(.clock(clk), .AddressS1(mem_intf.AddressS1), .AddressS2(mem_intf.AddressS2), .S1(mem_intf.S1), .S2(mem_intf.S2));
   
   assign memR_u.Rmem = mem_intf.R_mem;
   assign memS_u.Smem = mem_intf.S_mem;
 
-  test Motion_Estimator(mem_intf);      // Test //,memR_u, memS_u
+  test Motion_Estimator(mem_intf);  // Test instantiation
 
   initial begin
     $vcdpluson();
-    $dumpfile("dump.vcd"); $dumpvars;
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
   end
 
-  top dut(                              //DUT Instantiation
+  top dut(  // DUT Instantiation
     .clock(mem_intf.clk), 
     .start(mem_intf.start), 
     .BestDist(mem_intf.BestDist), 
@@ -43,16 +43,16 @@ module top_tb();
     .R(mem_intf.R), 
     .S1(mem_intf.S1), 
     .S2(mem_intf.S2), 
-    .completed(mem_intf.completed));
+    .completed(mem_intf.completed)
+  );
 
-  bind dut MotionEstimationAssertions(     // Binding Assertion to Top module
-      .clock(mem_intf.clk), 
-      .start(mem_intf.start), 
-      .BestDist(mem_intf.BestDist), 
-      .motionX(mem_intf.motionX), 
-      .motionY(mem_intf.motionY),  
-      .completed(mem_intf.completed)
-    );
+  bind dut MotionEstimationAssertions(  // Binding Assertion to Top module
+    .clk(mem_intf.clk), 
+    .trigger(mem_intf.start), 
+    .distance(mem_intf.BestDist), 
+    .vectorX(mem_intf.motionX), 
+    .vectorY(mem_intf.motionY),  
+    .done(mem_intf.completed)
+  );
 
- 
 endmodule
